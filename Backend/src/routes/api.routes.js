16eaa3b362bd725613d4e8926879f988867d286c
Enrique Avendaño Ardila <wenriqueaa@gmail.com>
2024-12-30 20:12:11 -0500

    eaa: modifies include swagger with validate token for Backend

--- Backend/src/routes/api.routes.js
+++ Backend/src/routes/api.routes.js
@@ -4,6 +4,6 @@ const client = require('./client.routes')
 const user = require('./user.routes')
 
 router.use('/api', client)
-// router.use('/api', user)
+router.use('/api', user)
 
 module.exports = router
