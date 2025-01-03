7e664f330a6b2641406fe59c2aaff201b7cf6cab
Enrique Avendaño Ardila <wenriqueaa@gmail.com>
2025-01-03 02:37:34 -0500

    EAA: add schemas for workorder, workordertask, taskevidences, tasktemplate, taskevidence

---
+++ Backend/src/models/TaskTemplate.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const TaskTemplateSchema = new mongoose.Schema({
+    serviceType: {
+        type: String,
+        enum: ['inspection', 'installation', 'maintenance'],
+        required: true
+    },
+    taskTemplateOrdering: {
+        type: Number
+    },
+    taskTemplateDescription: {
+        type: String,
+        required: true
+    },
+    taskTemplateSuggestedEvidence: {
+        type: String
+    }
+},
+    {
+        timestamps: true
+    });
+
+TaskTemplateSchema.pre('save', async function (next) {
+    if (this.isNew) {
+        const lastTaskTemplate = await mongoose.model('TaskTemplate').findOne({ serviceType: this.serviceType }).sort('-taskTemplateOrdering');
+        this.taskTemplateOrdering = lastTaskTemplate ? lastTaskTemplate.taskTemplateOrdering + 1 : 1;
+    }
+    next();
+});
+
+module.exports = mongoose.model('TaskTemplate', TaskTemplateSchema);
