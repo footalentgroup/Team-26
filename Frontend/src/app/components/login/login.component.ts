4c1312d5df2c50a7741ff9463f3afcf525f665db
Enrique Avendaño Ardila <wenriqueaa@gmail.com>
2025-01-08 18:05:57 -0500

    update for release

--- Frontend/src/app/components/login/login.component.ts
+++ Frontend/src/app/components/login/login.component.ts
@@ -4,11 +4,7 @@ import { MensajesService } from '../../services/mensajes.service';
 import { Router} from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { MensajesComponent } from "../mensajes/mensajes.component";
-<<<<<<< HEAD
-import Swal from 'sweetalert2';
-=======
 import { CommonModule } from '@angular/common';
->>>>>>> develop
 
 @Component({
   selector: 'app-login',
@@ -18,18 +14,6 @@ import { CommonModule } from '@angular/common';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-<<<<<<< HEAD
-
-  constructor(private peticion: PeticionService, private msg: MensajesService, private router: Router) {
-    this.verificarAdmin();
-  }
-
-  email: string = ""
-  password: string = ""
-
-  // Función para verificar si existe un administrador
-  async verificarAdmin() {
-=======
   
   constructor(private peticion:PeticionService, private msg:MensajesService, private router: Router){
     this.verificarAdmin(); 
@@ -53,10 +37,9 @@ export class LoginComponent {
     /*$$$$$$$$$$$$$$$$$$ FUNCION PARA VERIFICAR ADMINISTRADOR $$$$$$$$$$$
     $$$$$$$$$$$$$$$$$*/
     async verificarAdmin() {
->>>>>>> develop
     let data = {
       host: this.peticion.UrlHost,
-      path: "/api/usercheckadmin"
+      path: "/api/usercheckadmin" 
     };
 
     try {
@@ -64,7 +47,7 @@ export class LoginComponent {
       if (!res.hasAdministrator) {
         this.msg.Load("info", "No existe administrador. Redirigiendo al registro..");
         setTimeout(() => {
-          this.router.navigate(['/register-admin']);
+          this.router.navigate(['/register-admin']); 
         }, 3000);
       }
     } catch (error) {
@@ -72,75 +55,6 @@ export class LoginComponent {
       this.msg.Load("danger", "Error al conectar con el servidor.");
     }
   }
-<<<<<<< HEAD
-  Login() {
-    let message: string = ""
-    if (this.email == "" || this.password == "") {
-      switch (true) {
-        case this.email === "" && this.password === "":
-          message = "El correo y la contraseña son requeridos";
-          break;
-        case this.email === "" && this.password === "":
-          message = "El correo es requerido";
-          break;
-        case this.password === "":
-          message = "La contraseña es requerida";
-          break;
-      }
-      Swal.fire({
-        icon: "error",
-        title: "Oops...",
-        text: message,
-        footer: '<b>Intente nuevamente</a>'
-      });
-    }
-    else {
-      /*$$$$$$$$$$$$$$$$$$ ESTRUCTURA DE LA PETICIÓN $$$$$$$$$$$$$$$$$$$$$$$$$$$$*/
-      let data = {
-        host: this.peticion.UrlHost,
-        path: "/api/userlogin",
-        payload: {
-          email: this.email,
-          password: this.password
-
-        }
-      }
-      this.peticion.Post(data.host + data.path, data.payload).then((res: any) => {
-        console.log("holla vamos aqui", res)
-        if (!res.ok) {
-          this.msg.Load("danger", res.msg || "Error en el login");
-        }
-        else
-          if (res.userRole === "administrator") {
-            this.msg.Load("dark", res.userName || "Bienvenido");
-            this.router.navigate(["/dashboardadmin"]);
-          }
-          else if (res.userRole === "supervisor") {
-            this.msg.Load("dark", res.userName || "Bienvenido");
-            this.router.navigate(["/dashboardsup"]);
-          } else {
-            this.msg.Load("dark", res.userName || "Bienvenido");
-            this.router.navigate(["/"]);
-          }
-      }).catch((error: any) => {
-        console.log(error);
-        //        const {errorMessage } = error.error ? error.error.msg : "Error en el login";
-        const { errorMessage } = error
-        Swal.fire({
-          icon: "error",
-          title: "Oops...",
-          text: errorMessage,
-          footer: '<b>Intente nuevamente</a>'
-        });
-
-      });
-    }
-  }
-}
-
-
-
-=======
   /*$$$$$$$$$$$$$$$$$$ MÉTODO PARA ALTERNAR LA VISIBILIDAD DE LA CONTREASEÑA $$$$$$$$$$$$$$$$$$$$$$$*/
   togglePasswordVisibility(): void {
     this.showPassword = !this.showPassword;
@@ -176,7 +90,6 @@ export class LoginComponent {
         return;
       }
 
-
       this.msg.Load("dark", res.msg || "Bienvenido");
 
       localStorage.setItem("token", res.token);
@@ -192,15 +105,5 @@ export class LoginComponent {
       this.msg.Load("danger", "Error en el servidor. Intente nuevamente más tarde.");
     });
   }
-
-      else {
-        this.msg.Load("dark", res.msg || "Bienvenido");
-        this.router.navigate(["/dashboardsup"]);
-      }
-  
-  })
 }
 
-}
-
->>>>>>> develop
