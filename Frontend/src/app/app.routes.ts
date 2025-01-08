d3a5f03b9e5dc7feacd7fecffa0ce2584f7ad3fb
JuanCa291994 <jcrr9423@gmail.com>
2025-01-08 11:06:48 -0500

    Revert "Feature weaa 20250107 002"

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -3,12 +3,6 @@ import { DashboardadminComponent } from './components/dashboardadmin/dashboardad
 import { DashboardtecniComponent } from './components/dashboardtecni/dashboardtecni.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterAdminComponent } from './components/register-admin/register-admin.component';
-import { DashboardsupComponent } from './components/dashboardsup/dashboardsup.component';
-import { NavbarsupComponent } from './components/navbarsup/navbarsup.component';
-import { MapComponent } from './components/map/map.component';
-import { AgendaComponent } from './components/agenda/agenda.component';
-import { CalendarComponent } from './components/calendar/calendar.component';
-
 
 
 export const routes: Routes = [
@@ -16,12 +10,6 @@ export const routes: Routes = [
     {path:"login",component:LoginComponent,pathMatch:"full"},
     {path:"dashboardadmin",component:DashboardadminComponent,pathMatch:"full"},
     {path:"dashboardtecni",component:DashboardtecniComponent,pathMatch:"full"},
-    {path: "dashboardsup", component: DashboardsupComponent },
-    {path: "navbarsup", component: NavbarsupComponent }, 
-    {path: "map", component: MapComponent }, 
-    {path: "agenda", component: AgendaComponent },
-    {path: "calendar", component: CalendarComponent },  
-
-
+    {path: "register-admin", component: RegisterAdminComponent }, 
     {path: "**", redirectTo: 'login' } 
 ];
