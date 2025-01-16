3f48e8db186243203993f5394bbb93969b6e05ae
JuanCa291994 <jcrr9423@gmail.com>
2025-01-15 21:37:29 -0500

    add function navbarsup

--- Frontend/src/app/components/navbarsup/navbarsup.component.ts
+++ Frontend/src/app/components/navbarsup/navbarsup.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { PeticionService } from '../../services/peticion.service';
+
 
 @Component({
   selector: 'app-navbarsup',
@@ -7,6 +10,54 @@ import { Component } from '@angular/core';
   templateUrl: './navbarsup.component.html',
   styleUrl: './navbarsup.component.css'
 })
-export class NavbarsupComponent {
+export class NavbarsupComponent { 
+  constructor(private router: Router,private peticion:PeticionService){}
+  userName: string = '';
+  userRole: string = '';
 
+  ngOnInit() {
+    // Recuperar los datos desde el localStorage
+    this.userName = localStorage.getItem('userName') || 'Usuario'; 
+    this.userRole = localStorage.getItem('userRole') || '';
+  }
+  
+
+  navegar(ruta: string): void {
+      this.router.navigate([ruta]);
+  }
+  logout() {
+    console.log("Cerrando sesión del usuario");
+
+    const userId = localStorage.getItem('userId'); 
+    const url = `${this.peticion.UrlHost}/api/userclosesession/${userId}`; 
+  
+    if (userId) {
+  
+      this.peticion.Patch(url, {}).subscribe({
+        next: (res: any) => {
+          if (res.ok) {
+        
+            localStorage.removeItem('token');
+            localStorage.removeItem('userId');
+            localStorage.removeItem('userRole');
+            localStorage.removeItem('userName');
+  
+            
+            this.router.navigate(['/login']);
+            console.log('Sesión cerrada exitosamente.');
+          } else {
+            console.error('Error al cerrar sesión:', res.message);
+          }
+        },
+        error: (error) => {
+          
+          console.error('Error en la solicitud de logout:', error);
+        }
+      });
+    } else {
+      
+      console.warn('No se encontró un ID de usuario válido. Redirigiendo al login...');
+      this.router.navigate(['/login']);
+    }
+  }
 }
